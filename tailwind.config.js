b8c15452e76752d76fdc34d9786a69cb605525c5
Benja Netan <netanyahubenjamin70@gmail.com>
2025-02-04 12:50:33 +0300

    Refactor CSS and remove unused files; integrate Tailwind CSS with updated configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 // tailwind.config.js
 module.exports = {
     purge: [],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+        },
+        gridTemplateColumns: {
+          '70/30': '70% 28%',
+        },
+      },
     },
     variants: {
       extend: {},
