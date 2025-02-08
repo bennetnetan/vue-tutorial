f7f65149e8f0838dda01339704ed366c978bd697
Benja Netan <netanyahubenjamin70@gmail.com>
2025-02-08 18:33:56 +0300

    Add job management features with toast notifications; implement API proxy and create BackButton component
    Project complete

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -21,7 +21,7 @@
 
     onMounted(async () => {
         try {
-            const response = await axios.get('http://localhost:3001/jobs');
+            const response = await axios.get('/api/jobs');
             state.jobs = response.data;
         } catch (error) {
             console.error("Error fetching jobs", error);
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 import JobView from "@/views/JobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +23,16 @@ const router = createRouter({
             path: '/jobs/:id',
             name: 'job',
             component: JobView
+        },
+        {
+            path: '/jobs/add',
+            name: 'add-job',
+            component: AddJobView
+        },
+        {
+            path: '/jobs/edit/:id',
+            name: 'edit-job',
+            component: EditJobView
         },
         {
             path: '/:catchall(.*)',
